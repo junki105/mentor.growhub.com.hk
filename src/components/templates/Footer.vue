16fc699dcd38fa6fd7a1d84d7cc260e5b1803f58
junki105 <junki.dev@gmail.com>
2019-09-12 11:40:59 +0800

    refactor: added no-ssr tag to netlify form.

--- src/components/templates/Footer.vue
+++ src/components/templates/Footer.vue
@@ -1,5 +1,58 @@
 <template>
-  <footer>    
+  <footer>
+    <no-ssr>
+      <div v-show="state.needContactForm" class="flex justify-center px-2">
+        <div
+          class="bg-gradient-blue-to-green rounded-lg w-full p-4 mb-12 lg:py-16 lg:w-2/3"
+        >
+          <p class="text-center text-white text-lg lg:text-2xl mb-4">
+            お問い合わせはこちらから
+          </p>
+
+          <form
+            name="contact"
+            action="/contact/thanks"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <div class="flex flex-col lg:flex-row">
+              <div class="lg:flex-1 px-1 mb-2">
+                <input
+                  type="text"
+                  name="name"
+                  class="w-full rounded-lg p-4 border text-gray-800 border-gray-200"
+                  placeholder="山田 太郎"
+                />
+              </div>
+              <div class="lg:flex-1 px-1 mb-2">
+                <input
+                  type="email"
+                  name="email"
+                  class="w-full rounded-lg p-4 border text-gray-800 border-gray-200"
+                  placeholder="your@mail.com"
+                />
+              </div>
+            </div>
+            <div class="px-1 mb-2">
+              <textarea
+                name="content"
+                class="w-full rounded-lg p-4 border text-gray-800 border-gray-200"
+                col="6"
+              />
+            </div>
+            <div class="flex justify-center items-center px-1">
+              <button
+                class="w-full py-5 px-2 rounded-lg bg-indigo-700  text-gray-100 font-bold uppercase border-indigo-700 border-t border-b border-r lg:px-4 hover:opacity-75"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </no-ssr>
     <div class="bg-gray-800 text-gray-100 px-2 pt-0 pb-2 mt-0">
       <div class="container mx-auto py-8">
         <div class="flex justify-center text-xs">
