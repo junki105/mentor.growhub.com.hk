fe1b65ba4a0b7f605351dbf9916e7915b62fa1ad
junki105 <junki.dev@gmail.com>
2019-09-18 19:28:10 +0800

    feat: added intruduction section.

---
+++ src/components/organisms/home/IntroductionSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mb-8">
+    <div class="container mx-auto">
+      <div class="flex flex-col lg:flex-row items-center">
+        <div class="flex-1 px-2">
+          <h1 class="text-3xl text-center mb-4 lg:text-5xl lg:text-left">
+            GrowHub Mentor
+          </h1>
+          <p
+            class="font-semibold text-lg text-gray-800 text-center mb-2 lg:text-2xl lg:text-left"
+          >
+            あなたのWebプログラミングをサポートしていきます
+          </p>
+          <p class="text-sm text-gray-800 lg:text-base ">
+            Webデベロッパー / Laravel デベロッパー Codeメンター
+            として活動しています。<br />
+            今ままでの開発経験を生かして、プログラミング初学者に向けメンターをしてきます。<br />
+            また、Web開発のサポートが必要な企業に向けて開発サポートも行なっていきます。
+          </p>
+        </div>
+        <div class="flex-1 px-2">
+          <img
+            src="~/assets/images/goal.jpg"
+            alt="Goals"
+            class="w-full mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <introduction-section />
     <mentor-section />
     <how-to-use-section />
   </div>
@@ -7,11 +8,13 @@
 
 <script lang="ts">
 import { createComponent } from '@vue/composition-api'
+import IntroductionSection from '~/components/organisms/home/IntroductionSection.vue'
 import MentorSection from '~/components/organisms/home/MentorSection.vue'
 import HowToUseSection from '~/components/organisms/home/HowToUseSection.vue'
 
 export default createComponent({
   components: {
+    IntroductionSection,
     MentorSection,
     HowToUseSection
   }
